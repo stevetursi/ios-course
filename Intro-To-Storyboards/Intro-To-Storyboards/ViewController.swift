7183bcdfe5e1651347bfd4c8bbb5bdf9bde65ec3
Steven Tursi <stevetursi@gmail.com>
2016-06-20 21:20:26 -0400

    initial setup, first two views complete

---
+++ Intro-To-Storyboards/Intro-To-Storyboards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intro-To-Storyboards
+//
+//  Created by Lovey on 6/20/16.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
